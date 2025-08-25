2dc8ce6f83c577db35c05802e703de2415aa3f3b
Bekend10 <tiendungk18a1@gmail.com>
2025-08-25 16:45:17 +0700

    🚑 fix error authorize

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { useAuthStore } from '@/stores/authStore'
 import App from './App.vue'
 import router from './router'
 import './style.css'
@@ -9,8 +8,5 @@ const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
-const authStore = useAuthStore()
-await authStore.initializeAuth() 
 app.use(router)
-
 app.mount('#app')
